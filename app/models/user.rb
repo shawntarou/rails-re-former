41e2fec26b41953f6568f879b5b4887a04532355
shawn <173310929+shawntarou@users.noreply.github.com>
2026-09-10 16:20:20 -0700

    Add User form with rails methods and setup controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
-  validates :username, :email, :password,
+  validates :username, :password,
             length: { minimum: 2, maximum: 16 },
             uniqueness: true,
             presence: true,
             format: { with: /\A[a-zA-Z0-9]+\z/ } # a-z, A-Z, 0-9, no spaces
+  validates :email,
+            length: { minimum: 4, maximum: 16 },
+            uniqueness: true,
+            presence: true,
+            format: { with: /\A[a-zA-Z0-9@]+\z/ } # a-z, A-Z, 0-9, @, no spaces
 end
