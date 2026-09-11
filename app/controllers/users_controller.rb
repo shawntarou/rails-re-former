f140c55188cf041124b666e7d2edf35d08f57911
shawn <173310929+shawntarou@users.noreply.github.com>
2026-09-10 17:29:13 -0700

    Add edit User form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ edit update ]
+
   def new
     @user = User.new
   end
@@ -13,7 +15,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.expect(user: [ :username, :email, :password ])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :users, only: [ :new, :create ]
+  resources :users, only: [ :new, :create, :edit, :update ]
   root "users#new"
 end
